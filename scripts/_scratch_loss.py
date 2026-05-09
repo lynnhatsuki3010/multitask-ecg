4d4b7625d2598e1015c7d0b38d74dae7f4f531d7
lynnhatsuki3010 <lyn.nhi3010@gmail.com>
2026-05-09 12:38:54 +0700

    docs: add ASL results to Stage 3

--- scripts/_scratch_loss.py
+++ scripts/_scratch_loss.py
@@ -1,9 +1,7 @@
 import json, os, yaml
 
 runs = [
-    'run_20260508_005129_hybrid-tf',
-    'run_20260508_015300_hybrid-tf-focal',
-    'run_20260508_025238_hybrid-tf-focal',
+    'run_20260509_110546_hybrid-tf',
 ]
 keys = ['auroc/mi/IMI','auprc/mi/IMI','f1/mi/IMI','auroc/arrhy/macro','f1/arrhy/macro']
 
@@ -14,10 +12,7 @@ for run in runs:
         with open(cfg_path, 'r', encoding='utf-8') as f:
             cfg = yaml.safe_load(f)
             t = cfg.get('training', {})
-            pw = t.get('use_pos_weight', False)
-            foc = t.get('use_focal', False)
-            grad = cfg.get('model', {}).get('mi_gradient_scale', 1.0)
-            print(f'  pos_weight: {pw}, focal: {foc}, grad_scale: {grad}')
+            print(f"  use_asl: {t.get('use_asl', False)}")
             
     met_path = os.path.join('checkpoints', run, 'test_metrics.json')
     if os.path.exists(met_path):
